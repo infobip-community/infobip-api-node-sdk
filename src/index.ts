b7d538c63b49c36f620959b7646f009d80ba3caf
Alex Lakatos <alex.lakatos.qa@gmail.com>
2022-05-12 22:40:11 +0200

    add validation for the Auth API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { AuthType } from './utils/auth-type';
 import { InfobipAuth } from './utils/auth';
+import { Validator } from './utils/validator';
 import { WhatsApp } from './apis/whatsapp';
 import { Auth } from './apis/auth';
+
 
 class Infobip {
   /**
@@ -23,6 +25,16 @@ class Infobip {
     oauthToken,
     ibssoToken,
   }: InfobipAuth) {
+    
+    Validator.required(baseUrl, 'Infobip.baseUrl')
+    Validator.required(authType, 'Infobip.authType')
+    
+    password && Validator.string(password, 'Infobip.password');
+    username && Validator.string(username, 'Infobip.username');
+    apiKey && Validator.string(apiKey, 'Infobip.apiKey');
+    oauthToken && Validator.string(oauthToken, 'Infobip.oauthToken');
+    ibssoToken && Validator.string(ibssoToken, 'Infobip.ibssoToken');
+
     this.credentials = new InfobipAuth({
       baseUrl,
       authType,
