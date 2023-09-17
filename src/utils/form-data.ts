8b84264e4ac508c211f92bf4e10be0d242adc24a
Mike Elsmore <mike.elsmore@infobip.com>
2023-09-17 16:13:42 +0200

    Linting error correction

--- src/utils/form-data.ts
+++ src/utils/form-data.ts
@@ -3,7 +3,11 @@ export function FormDataBuilder(formData: any, data: any, parentKey?: string) {
     Object.keys(data).forEach(key => {
       if (key === 'attachment' || key === 'inlineImage') {
         data[key].forEach((attachedObject: any) => {
-          formData.append((parentKey ? `${parentKey}['${key}']` : key), attachedObject.data, attachedObject.name);
+          formData.append(
+            parentKey ? `${parentKey}['${key}']` : key,
+            attachedObject.data,
+            attachedObject.name
+          );
         });
       } else {
         FormDataBuilder(
